98413c238c7eb3cdae8fc5a604f605c1e7f2bb30
Augusto <jesus_augga_99@icloud.com>
2022-07-06 16:10:10 -0500

    add after save calls

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  after_save :update_post_counter
+
+  private
+
   def update_post_counter
     author.update(post_counter: author.posts.count)
   end
