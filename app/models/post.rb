20ad247bfa39c08afa4575db7bda5b331781a9a0
Augusto <jesus_augga_99@icloud.com>
2022-07-06 14:28:17 -0500

    add last 5 comments method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def last_5_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
+
+post = Post.first
