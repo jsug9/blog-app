6509dd0c81daf5159db74f2911641a9735855da8
Augusto <jesus_augga_99@icloud.com>
2022-07-06 14:33:45 -0500

    add method to updated post counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  def update_post_counter
+    author.update(post_counter: author.posts.count)
+  end
+
   def last_5_comments
     comments.order(created_at: :desc).limit(5)
   end
