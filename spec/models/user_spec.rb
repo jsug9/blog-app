80b1a52986576812274f2c3872e4d6894676bb17
Augusto <jesus_augga_99@icloud.com>
2022-07-07 11:20:46 -0500

    add tests for user methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,13 +6,41 @@ RSpec.describe User, type: :model do
     @user.save
   end
 
-  it 'name should be present' do
-    @user.name = nil
-    expect(@user).to_not be_valid
+  describe 'User Model Properties' do
+    it 'name should be present' do
+      @user.name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'should have a photo' do
+      @user.photo = 'photo'
+      expect(@user).to be_valid
+    end
+
+    it 'should have a bio' do
+      @user.bio = 'bio'
+      expect(@user).to be_valid
+    end
+
+    it 'post_counter should be greater than or equal to 0' do
+      @user.post_counter = nil
+      expect(@user).to_not be_valid
+    end
   end
 
-  it 'post_counter should be greater than or equal to 0' do
-    @user.post_counter = nil
-    expect(@user).to_not be_valid
+  describe 'User Model Methods' do
+    before do
+      5.times do |n|
+        Post.create(title: "Post #{n}", author_id: @user.id, comments_counter: 0, likes_counter: 0)
+      end
+    end
+
+    it 'should have a last_3_posts method' do
+      expect(@user).to respond_to(:last_3_posts)
+    end
+
+    it 'should return the last 3 posts' do
+      expect(@user.last_3_posts.count).to eq(3)
+    end
   end
 end
