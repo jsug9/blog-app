e73a4ea6fc1e4a71db06712a8417c9c83865451a
Augusto <jesus_augga_99@icloud.com>
2022-07-07 12:03:30 -0500

    add tests for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,13 +8,13 @@ class Post < ApplicationRecord
 
   after_save :update_post_counter
 
+  def last_5_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
   private
 
   def update_post_counter
     author.update(post_counter: author.posts.count)
-  end
-
-  def last_5_comments
-    comments.order(created_at: :desc).limit(5)
   end
 end
