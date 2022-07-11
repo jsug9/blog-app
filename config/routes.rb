9c3f831a4b9ad3ce7c451cd3360b3673679cf993
Augusto <jesus_augga_99@icloud.com>
2022-07-11 11:03:20 -0500

    refactor routes to make it shorter

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
 
   root "users#index"
 
-  get '/users/:author_id/posts' => 'posts#index', as: :user_posts
-  get '/users/:author_id/posts/:id' => 'posts#show', as: :user_post
-  get '/users' => 'users#index', as: :users
-  get '/users/:id' => 'users#show', as: :user
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
