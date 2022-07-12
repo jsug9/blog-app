8a1ea7bd937fcfac20d83c318466c4e3a46d2cfc
Augusto <jesus_augga_99@icloud.com>
2022-07-12 16:06:55 -0500

    test comments form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,11 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    new_comment = current_user.comments.new(
-      text: comment_params,
-      user_id: current_user.id,
-      post_id: @post.id
-    )
+    @comment = Comment.new(comments_params)
+    @comment.author_id = current_user
+    @comment.post_id = @post
 
-    if new_comment.save
+    if @comment.save
       redirect_to user_post_path(current_user, @post)
     else
       render :new
