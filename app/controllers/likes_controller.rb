4dbb0d8519c1f548c2fea87f48b9b87bb88e00c5
Augusto <jesus_augga_99@icloud.com>
2022-07-12 12:17:31 -0500

    add likes controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,20 @@
+class LikesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    if already_liked?
+      redirect_to user_post_path(@user.id, @post)
+    else
+      new_like = current_user.likes.new(
+        user_id: current_user.id,
+        post_id: @post.id
+      )
+      if new_like.save
+        redirect_to user_post_path(@user.id, @post)
+      end
+    end
+  end
+
+  def already_liked?
+    Like.where(author_id: current_user.id, post_id: params[:post_id]).exists?
+  end
+end
