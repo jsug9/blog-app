4c54820cf4c42c2fae74fffaba9306bbd71021d8
Augusto <jesus_augga_99@icloud.com>
2022-07-12 12:11:15 -0500

    update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,22 @@ class PostsController < ApplicationController
     @user = @post.author
     @comments = @post.comments
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(posts_params)
+    @post.author = current_user
+    if @post.save
+      redirect_to user_post_path(current_user, @post)
+    else
+      render :new
+    end
+  end
+
+  def posts_params
+    params.require(:post).permit(:title, :text)
+  end
 end
