2b2e85e63bbe877846693bf51ab7d958799eeb7a
Augusto <jesus_augga_99@icloud.com>
2022-07-21 08:29:41 -0500

    fix user name error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  include Response
+  include ExceptionHandler
+  protect_from_forgery with: :exception, unless: :json_request
 
   before_action :update_allowed_parameters, if: :devise_controller?
   before_action :authorize_request, if: :json_request
