ccff439ab84ffcdcea21aed1db7be60578926440
Augusto <jesus_augga_99@icloud.com>
2022-07-20 23:40:10 -0500

    add create method to comment api

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -3,4 +3,21 @@ class Api::CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     render json: @post.comments
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @comment = @user.comments.new(comments_params)
+    @comment.author_id = @user.id
+    @comment.post_id = params[:post_id]
+    response = if @comment.save
+                 { comment: @comment }
+               else
+                 { message: "Comment didn't save" }
+               end
+    json_response(response)
+  end
+
+  def comments_params
+    params.permit(:text, :post_id)
+  end
 end
